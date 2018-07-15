2928ec34c2b3c3bf170779c7997d3224f89a3cae
YanChunlin96 <1519780292@qq.com>
2018-07-15 14:50:23 +0800

    xiaoyitong ycl

---
+++ js/city.data.js
@@ -0,0 +1,31 @@
+var cityData = [{
+	value: '110000',
+	text: '大一',
+	children: [{
+		value: "110101",
+		text: "第一学期"
+	}, {
+		value: "110102",
+		text: "第二学期"
+	}]
+}, {
+	value: '120000',
+	text: '大二',
+	children: [{
+		value: "120101",
+		text: "第一学期"
+	}, {
+		value: "120102",
+		text: "第二学期"
+	}]
+}, {
+	value: '130000',
+	text: '大三',
+	children: [{
+		value: "130100",
+		text: "第一学期"
+	}, {
+		value: "1301001",
+		text: "第二学期"
+	}]
+}]
